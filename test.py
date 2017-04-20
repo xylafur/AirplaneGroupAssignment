9d10d7882db47b13d9a2ac16f29ce984f0b45ed8
xylafur <jkesleyrichardson@gmail.com>
2017-04-20 15:56:09 -0500

    proof of concept python

--- test.py
+++ test.py
@@ -3,12 +3,15 @@ class City:
         self.outgoingFlights = []
         self.name = name
 
+    def __str__(self):
+        return "{}   Outgoing Flights: {}".format(
+                self.name, [ flight.__str__() for flight in self.outgoingFlights ] )
     def addFlight(self, destination, distance, price):
         temp = Flight( destination, distance, price)
         self.outgoingFlights.append( temp )
 
     def printFlights(self):
-        print(
+        pass
 
 class Flight:
     def __init__(self, destination, distance, price):
@@ -18,12 +21,25 @@ class Flight:
 
     def __str__(self):
         return (
-            "Destination: {}\t Distance: {}\tPrice: {}".format(
+            "Destination: {}   Distance: {}   Price: {}".format(
             self.destination.name,  self.distance, self.price) )
-
+    
 class Airline:
     def __init__(self):
         self.cities = []
+    
+    def __str__(self):
+        ret = "Airline Object   Cities: {}".format(
+                [city.__str__() for city in self.cities ])
+        return ret
+
+    def print(self):
+        print("*"*23)
+        print("*Airline with cities: *")
+        print("*"*23)
+        for city in self.cities:
+            print("*{}".format(city))
+        print("*"*20)
 
     def addFlight(self, origin, destination, distance, price):
         origin_ptr = None;  destination_ptr = None
@@ -36,11 +52,18 @@ class Airline:
 
         if origin_ptr == None:
             origin_ptr = City( origin )
-            cities.append( origin_ptr )
+            self.cities.append( origin_ptr )
 
         if destination_ptr == None:
             destination_ptr = City( destination )
-            cities.append( City(destination_ptr) )
+            self.cities.append( City(destination_ptr) )
 
         #now both the destination and origin are created
         origin_ptr.addFlight( destination_ptr, distance, price )
+
+
+cosc = Airline()
+
+cosc.addFlight("Houston", "Las Vegas", 500, 200)
+
+cosc.print()
