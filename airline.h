0b612ca43886dafca265348159d067d3f9ec642c
xylafur <jkesleyrichardson@gmail.com>
2017-04-22 15:55:44 -0500

    getting farther along, still compiles but havent tested at all haha

--- airline.h
+++ airline.h
@@ -4,8 +4,11 @@
 #include <string>
 #include <iostream>
 
+class City;
+
 struct Flight{
     std::string destination;
+    City* city;
     int distance;
     int price;
 };
@@ -34,14 +37,41 @@ public:
                 <<outgoingFlights[i].price<<std::endl;
         }
     }
+    std::string getName(){
+        return this->name;
+    }   
 };
 
 class Airline{
 private:
-    std::vector<City> cities;
+    std::vector<City *> cities;
 
 public:
-    Airline(){
+    Airline(){}
+    void addFlight(std::string origin, std::string destination,
+            int distance, int price){
+        City *origin_ptr = NULL, *destination_ptr = NULL;
+        //loop through all the cities to see if the origin and destination are
+        //already defined
+        for( int i = 0; i < cities.size(); i++){
+            if( origin.compare( cities[i]->getName() ) == 0)
+                origin_ptr = cities[i];
+            if( destination.compare( cities[i]->getName() ) == 0)
+                destination_ptr = cities[i];
+        }
+        //if the cities don't exist in the cities list we will create and append
+        //them
+        if( origin_ptr == NULL){
+            origin_ptr = new City(origin);
+            this->cities.push_back(origin_ptr);
+        }
+        if( destination_ptr == NULL){
+            destination_ptr = new City(destination);
+            this->cities.push_back(destination_ptr);
+        }
+        //At this point both origin and destinatoin ptr are created and exist in
+        //the list of cities.  SO now we jsut add the destination as a outgoing
+        //flight for origin.
 
     }
 };
